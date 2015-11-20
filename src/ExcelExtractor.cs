d432143e87277403879a6cf20f39bc2e27d666e2
Szab <chlebak@windowslive.com>
2015-11-20 20:42:04 +0100

    Initial commit

---
+++ src/ExcelExtractor.cs
@@ -0,0 +1,24 @@
+﻿///-------------------------------------------------------------------------///
+///   Namespace:      Szab.ExcelExtractor                                   ///
+///   Class:          ExcelExtractor                                        ///
+///   Description:    A lib for extracting values from MSExcel files        ///
+///   Author:         Szab                              Date: 20.11.2015    ///
+///                                                                         ///
+///   Notes:                                                                ///
+///                                                                         ///
+///                                                                         ///
+///                                                                         ///
+///-------------------------------------------------------------------------///
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szab.ExcelExtractor
+{
+    public class ExcelExtractor
+    {
+    }
+}
